d5b7a675caa3fa97ab50f47bad5284d7f397f52f
Yago Dias <yago.dias@fluencypass.com>
2022-09-27 19:58:40 -0300

    Passing inside arrays to get a specified value by specific id

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,48 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import ReactHtmlParser from 'react-html-parser';
+
+const recurrencies = [
+  {
+    description: "Anual com 47% OFF",
+    frequency: "anual",
+    id: 18,
+    months: 12,
+    paymentType: "creditCard",
+    price: 189
+  },
+  {
+    description: "Mensal sem desconto",
+    frequency: "mensal",
+    id: 19,
+    months: 1,
+    paymentType: "creditCard",
+    price: 357
+  },
+  {
+    description: "Anual com 47% OFF",
+    frequency: "anual",
+    id: 23,
+    months: 12,
+    paymentType: "bankSlip",
+    price: 189
+  },
+]
 
 export default function Home() {
+  const recurrencyId = 18;
+  
+  function findMyRecurrency() {
+    let recurrency = 0;
+    
+    recurrencies.forEach(r => {if (r.id === recurrencyId) return recurrency = r.price * r.months});
+
+
+    return recurrency;
+  }
+
+  console.log(findMyRecurrency(recurrencies));
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,57 +53,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {findMyRecurrency(recurrencies)}
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
